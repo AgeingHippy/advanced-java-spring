30b8975bafd3ebe27051a3fc96c3af50347049bf
AgeingHippy <charles_mcdonald@hotmail.com>
2024-12-09 13:50:26 +0000

    xml config

--- src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
@@ -2,6 +2,7 @@
 package com.codingnomads.corespring.examples.application_context.xml;
 
 import java.text.MessageFormat;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +12,19 @@ public class CodingNomad {
     private final JDK jdk;
     private final IDE ide;
     private final Framework framework;
+    private final ALM alm;
 
     public String createAwesomeSoftware() {
         return MessageFormat.format(
                 "This coding nomad is creating awesome software using, "
-                        + "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5})",
+                        + "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5}), ALM: ({6}:{7})",
                 ide.getName(),
                 ide.getVersion(),
                 jdk.getName(),
                 jdk.getVersion(),
                 framework.getName(),
-                framework.getVersion());
+                framework.getVersion(),
+                alm.getName(),
+                alm.getVersion());
     }
 }
