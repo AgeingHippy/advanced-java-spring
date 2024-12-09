b29d0e78303a4ef04a5024a4a50cd6a1ed2db62b
AgeingHippy <charles_mcdonald@hotmail.com>
2024-12-09 14:58:20 +0000

    custom event listener

---
+++ src/main/java/com/codingnomads/corespring/examples/application_context/customeventlistener/SecondUserRegistrationCompletedEventListener.java
@@ -0,0 +1,14 @@
+package com.codingnomads.corespring.examples.application_context.customeventlistener;
+
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecondUserRegistrationCompletedEventListener implements ApplicationListener<UserRegistrationCompletedEvent> {
+
+    @Override
+    public void onApplicationEvent(UserRegistrationCompletedEvent event) {
+        System.out.println("I'm a second listener!");
+    }
+}
