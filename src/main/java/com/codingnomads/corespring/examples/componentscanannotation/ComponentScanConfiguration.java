be8f04473f1bfbe5f39b4cb7f5a8d85370c352a7
AgeingHippy <charles_mcdonald@hotmail.com>
2024-12-12 14:48:42 +0000

    ComponentScan filters

--- src/main/java/com/codingnomads/corespring/examples/componentscanannotation/ComponentScanConfiguration.java
+++ src/main/java/com/codingnomads/corespring/examples/componentscanannotation/ComponentScanConfiguration.java
@@ -4,9 +4,12 @@ package com.codingnomads.corespring.examples.componentscanannotation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "com.codingnomads.corespring.examples.componentscanannotation")
+@ComponentScan(basePackages = "com.codingnomads.corespring.examples.componentscanannotation",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
+                pattern = "com\\.codingnomads\\.corespring\\.examples\\.componentscanannotation\\.furniture\\..*"))
 public class ComponentScanConfiguration {
 
     @Bean
