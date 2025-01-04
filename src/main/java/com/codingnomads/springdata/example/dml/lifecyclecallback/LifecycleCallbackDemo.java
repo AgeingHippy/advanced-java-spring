b4622f81d2bca6c8991702a8eefa976d2b020509
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-04 12:33:45 +0000

    callback annotations

--- src/main/java/com/codingnomads/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
+++ src/main/java/com/codingnomads/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class LifecycleCallbackDemo {
 
@@ -17,6 +19,44 @@ public class LifecycleCallbackDemo {
     public CommandLineRunner runStuff(PrintEntityRepository printEntityRepository) {
         return (args) -> {
             // put your logic here
+            //create a few PrintEntities
+            System.out.println("=== CREATE ONE ENTITY AND SAVE ===");
+            PrintEntity printEntity1 = PrintEntity.builder().value("A").build();
+            printEntityRepository.save(printEntity1);
+
+            System.out.println("=== CREATE 3 ENTITIES ===");
+            ArrayList<PrintEntity> printEntities = new ArrayList<>();
+            printEntities.add( PrintEntity.builder().value("B").build());
+            printEntities.add( PrintEntity.builder().value("C").build());
+            printEntities.add( PrintEntity.builder().value("D").build());
+
+            System.out.println("=== SAVE 3 ENTITIES ===");
+            printEntityRepository.saveAll(printEntities);
+
+            System.out.println("=== UPDATE 1 ENTITY AND SAVE ===");
+            printEntity1.setValue("AA");
+            printEntityRepository.save(printEntity1);
+            System.out.println("printEntity1 = " + printEntity1);
+
+            System.out.println("=== FETCH 1 ENTITY ===");
+            PrintEntity printEntity2 = printEntityRepository.findById(printEntities.get(1).getId()).get();
+            System.out.println("printEntity2 = " + printEntity2);
+            System.out.println("== MODIFY ==");
+            printEntity2.setValue("CC");
+            System.out.println("printEntity2 = " + printEntity2);
+            System.out.println("printEntities2(1) = " + printEntities.get(1).toString());
+            System.out.println("== SAVE ==");
+            printEntityRepository.save(printEntity2);
+            System.out.println("printEntity2 = " + printEntity2);
+            System.out.println("printEntities2(1) = " + printEntities.get(1).toString());
+            System.out.println("== REMOVE ==");
+            printEntityRepository.delete(printEntity2);
+            System.out.println("printEntity2 = " + printEntity2);
+            System.out.println("printEntities2(1) = " + printEntities.get(1).toString());
+
+            //cleanup
+            System.out.println("=== Process CLEANUP (Delete remaining entities)===");
+            printEntityRepository.deleteAll();
 
         };
     }
--- src/main/java/com/codingnomads/springdata/example/dml/lifecyclecallback/PrintEntity.java
+++ src/main/java/com/codingnomads/springdata/example/dml/lifecyclecallback/PrintEntity.java
@@ -1,23 +1,58 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springdata.example.dml.lifecyclecallback;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Getter
-@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class PrintEntity {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column
+    private String value;
+
     // write your methods here
+    @PrePersist
+    public void myPrePersist() {
+        System.out.println("PrintEntity PrePersist " + this.toString() );
+    }
+
+    @PreUpdate
+    public void myPreUpdate() {
+        System.out.println("PrintEntity PreUpdate " + this.toString());
+    }
+
+    @PreRemove
+    public void myPreRemove() {
+        System.out.println("PrintEntity PreRemove " + this.toString());
+    }
+
+    @PostPersist
+    public void myPostPersist() {
+        System.out.println("PrintEntity PostPersist " + this.toString());
+    }
+
+    @PostUpdate
+    public void myPostUpdate() {
+        System.out.println("PrintEntity PostUpdate " + this.toString());
+    }
+
+    @PostRemove
+    public void myPostRemove() {
+        System.out.println("PrintEntity PostRemove " + this.toString());
+    }
+
+    @PostLoad
+    public void myPostLoad() {
+        System.out.println("PrintEntity PostLoad " + this.toString());
+    }
+
 
 }
