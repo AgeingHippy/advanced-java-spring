f12632889172378bce98c66d5ce4c5764bf4bcd3
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-07 13:04:23 +0000

    QueryDSL

--- src/main/java/com/codingnomads/springdata/example/querydsl/QueryDSLDemo.java
+++ src/main/java/com/codingnomads/springdata/example/querydsl/QueryDSLDemo.java
@@ -1,16 +1,15 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springdata.example.querydsl;
 
-import com.codingnomads.springdata.example.querydsl.models.Area;
-import com.codingnomads.springdata.example.querydsl.models.QArea;
-import com.codingnomads.springdata.example.querydsl.models.Route;
-import com.codingnomads.springdata.example.querydsl.models.SearchQuery;
+import com.codingnomads.springdata.example.querydsl.models.*;
 import com.codingnomads.springdata.example.querydsl.repository.AreaRepository;
 import com.codingnomads.springdata.example.querydsl.repository.RouteRepository;
 import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+
 import java.util.Arrays;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -41,7 +40,9 @@ public class QueryDSLDemo implements CommandLineRunner {
                 Area.builder().code("D").name("D").build(),
                 Area.builder().code("E").name("E").build(),
                 Area.builder().code("F").name("F").build(),
-                Area.builder().code("G").name("G").build()));
+                Area.builder().code("G").name("G").build(),
+                Area.builder().code("H").name("H").build(),
+                Area.builder().code("I").name("I").build()));
 
         final List<Route> routes = routeRepository.saveAll(Arrays.asList(
                 Route.builder()
@@ -95,6 +96,36 @@ public class QueryDSLDemo implements CommandLineRunner {
         JPAQuery<?> query = new JPAQuery<>(entityManager);
         Area area = query.select(qArea).from(qArea).where(qArea.code.eq("A")).fetchOne();
         System.out.println(area);
+
+        System.out.println("== Any route using area 'B' ==");
+        QRoute qRoute = QRoute.route;
+        query = new JPAQuery<>(entityManager);
+        List<Route> routes2 =
+                query.select(qRoute)
+                        .from(qRoute)
+                        .where(
+                                qRoute.origin.code.eq("B")
+                                        .or(qRoute.destination.code.eq("B"))
+                        )
+                        .fetch();
+        routes2.forEach(System.out::println);
+
+        System.out.println("== Only areas A,C,E and G");
+        query = new JPAQuery<>(entityManager);
+        List<Area> areas2 = query.select(qArea)
+                .from(qArea)
+                .where(qArea.code.in("A", "C", "E", "G"))
+                .fetch();
+        areas2.forEach(System.out::println);
+
+        System.out.println("== unused areas (left join) ==");
+        query = new JPAQuery<>(entityManager);
+        List<Area> areas3 = query.select(qArea)
+                .from(qArea)
+                .leftJoin(qRoute).on(qRoute.origin.eq(qArea).or(qRoute.destination.eq(qArea)))
+                .where(qRoute.id.isNull())
+                .fetch();
+        areas3.forEach(System.out::println);
 
         routeRepository.deleteAll();
         areaRepository.deleteAll();
