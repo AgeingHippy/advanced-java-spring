aeebeae19204804358bfde5c3bfe3a518f8df4ae
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-11 15:48:54 +0000

    Get spring rest template

--- src/main/java/com/codingnomads/springweb/resttemplate/GET/getForObject/GetForObjectDemo.java
+++ src/main/java/com/codingnomads/springweb/resttemplate/GET/getForObject/GetForObjectDemo.java
@@ -1,8 +1,14 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springweb.resttemplate.GET.getForObject;
 
+import com.codingnomads.springweb.resttemplate.GET.models.Excuse;
 import com.codingnomads.springweb.resttemplate.GET.models.QuoteTemplate;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -34,6 +40,32 @@ public class GetForObjectDemo {
             //                        CodingNomadsTasksApiResponse.class);
             //
             //        System.out.println(response.toString());
+
+            final String excuseBaseUrl = "https://excuser-three.vercel.app/v1/excuse";
+            Excuse[] excuses;
+            excuses = restTemplate.getForObject(excuseBaseUrl,Excuse[].class);
+            Arrays.asList(excuses).forEach(System.out::println);
+
+            System.out.println("== 3 Funny (?) excuses ==");
+            excuses = restTemplate.getForObject(excuseBaseUrl+"/funny/3", Excuse[].class);
+            Arrays.asList(excuses).forEach(System.out::println);
+
+            System.out.println("== 3 Office excuses ==");
+            excuses = restTemplate.getForObject(excuseBaseUrl+"/office/3", Excuse[].class);
+            Arrays.asList(excuses).forEach(System.out::println);
+
+            System.out.println("One particular excuse with id 102");
+            Excuse excuse = restTemplate.getForObject(excuseBaseUrl+"/id/102", Excuse.class);
+            System.out.println(excuse);
+
+            System.out.println("== fetching using params ==");
+            final String excuseBaseUrlWithParameters = excuseBaseUrl + "/{category}/{count}";
+            Map<String ,String> excusesParams = new HashMap<>();
+            excusesParams.put("category","gaming");
+            excusesParams.put("count","3");
+
+            excuses = restTemplate.getForObject(excuseBaseUrlWithParameters, Excuse[].class, excusesParams);
+            Arrays.asList(excuses).forEach(System.out::println);
 
         };
     }
---
+++ src/main/java/com/codingnomads/springweb/resttemplate/GET/models/Excuse.java
@@ -0,0 +1,11 @@
+package com.codingnomads.springweb.resttemplate.GET.models;
+
+import lombok.Data;
+
+@Data
+public class Excuse {
+
+    private long id;
+    private String excuse;
+    private String category;
+}
