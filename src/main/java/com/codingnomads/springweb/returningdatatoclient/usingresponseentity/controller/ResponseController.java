accfb6b4bc99de0fd36337d9d2e142b2e2be0cb8
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-13 20:18:39 +0000

    responseEntity

--- src/main/java/com/codingnomads/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
+++ src/main/java/com/codingnomads/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
@@ -3,9 +3,14 @@ package com.codingnomads.springweb.returningdatatoclient.usingresponseentity.con
 
 import com.codingnomads.springweb.returningdatatoclient.usingresponseentity.model.User;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.support.incrementer.MySQLMaxValueIncrementer;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +43,34 @@ public class ResponseController {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/practice")
+    public ResponseEntity<?> doPractice() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/practice/1")
+    public ResponseEntity<?> doPractice1() {
+        Map<String,String> myMap = new HashMap<>();
+        myMap.put("A","a");
+        myMap.put("C","c");
+        return ResponseEntity.ok().body(myMap);
+    }
+
+    @GetMapping("/practice/2")
+    public ResponseEntity<?> doPractice2() {
+        return ResponseEntity.status(200)
+                .header("H1","hache one")
+                .header("foo","bar")
+                .body("a random string");
+    }
+
+    @GetMapping("/practice/3")
+    public ResponseEntity<?> doPractice3() {
+        return ResponseEntity.status(201)
+                .header("H1","hache one","anotherH")
+                .header("foo","bar")
+                .body("a random string and some headers");
+    }
+
 }
