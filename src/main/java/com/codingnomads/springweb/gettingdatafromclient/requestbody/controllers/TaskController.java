9b7b585b8e44637085cb8e56792d5ecda23b5f0f
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-14 14:56:50 +0000

    RequestBody annotation

--- src/main/java/com/codingnomads/springweb/gettingdatafromclient/requestbody/controllers/TaskController.java
+++ src/main/java/com/codingnomads/springweb/gettingdatafromclient/requestbody/controllers/TaskController.java
@@ -3,16 +3,16 @@ package com.codingnomads.springweb.gettingdatafromclient.requestbody.controllers
 
 import com.codingnomads.springweb.gettingdatafromclient.requestbody.models.Task;
 import com.codingnomads.springweb.gettingdatafromclient.requestbody.repositories.TaskRepository;
+
 import java.net.URI;
 import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
@@ -35,6 +35,36 @@ public class TaskController {
                 .body(savedTask);
     }
 
+    @PutMapping("/api/tasks")
+    public ResponseEntity<Task> putTask(@RequestBody Task taskPut) {
+        if (taskPut.getId() == null || taskPut.getId() == 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (!taskRepository.existsById(taskPut.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        taskRepository.save(taskPut);
+
+        Task task = taskRepository.findById(taskPut.getId()).get();
+        return ResponseEntity.ok(task);
+    }
+
+    @PatchMapping("/api/tasks/{id}")
+    public ResponseEntity<?> putTask2(@RequestBody Task taskPatch, @PathVariable Long id) {
+        if (!taskRepository.existsById(id)) {
+            return ResponseEntity.badRequest().body("Task not found");
+        }
+
+        Task task = taskRepository.findById(id).get();
+
+        task.setName(taskPatch.getName() != null ? taskPatch.getName() : task.getName());
+        task.setCompleted(taskPatch.getCompleted() != null ? taskPatch.getCompleted() : task.getCompleted());
+
+        task = taskRepository.save(task);
+        return ResponseEntity.ok(task);
+    }
+
     @PostMapping(value = "/print")
     public ResponseEntity<?> printMessage(@RequestBody(required = false) String message) {
 
@@ -49,4 +79,6 @@ public class TaskController {
             return ResponseEntity.ok().body(message);
         }
     }
+
+
 }
